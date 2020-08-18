76ab36da50d86ce9cd9236de1363bd8949bece74
David Mendoza <gabrieldavid_98@hotmail.com>
2020-08-17 23:02:18 -0500

    Add files via upload

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
-(global-linum-mode 1)
+;; init.el -- Emacs conf
+;;; Commentary:
+;;; Code:
+
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
 (electric-pair-mode 1)
 (show-paren-mode 1)
 (ido-mode 1)
@@ -7,16 +12,20 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(setq backup-directory-alist '(("." . "~/emacs.d/backup"))
+(setq ido-enable-flex-matching t
+      ido-everywhere t)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
       backup-by-copying t
       version-control t
       delete-old-versions t
       kept-new-versions 5
       kept-old-versions 5)
 
-(set-default-font "Ubuntu Mono 12" nil t)
+;; (setq split-height-threshold nil
+;;       split-width-threshold 0)
+
+(set-frame-font "Ubuntu Mono 12" nil t)
 
 (defalias 'list-buffers 'ibuffer)
 
@@ -86,19 +95,14 @@
   :ensure t
   :config (add-hook 'after-init-hook 'global-company-mode))
 
-(use-package company-php
-  :defer
-  :after company)
-
 (use-package web-mode
   :ensure t
   :config
   (add-hook 'web-mode-hook 'my-web-mode-hook)
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode)))
-
-(setq web-mode-enable-auto-closing t)
+  (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
+  (setq web-mode-enable-auto-closing t))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
@@ -107,3 +111,82 @@
   (setq web-mode-code-indent-offset 3)
   (setq web-mode-attr-indent-offset 3))
 
+(use-package elixir-mode
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
+
+(use-package neotree
+  :ensure t
+  :config
+  (global-set-key [f8] 'neotree-toggle))
+
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
+(add-hook 'find-file-hook 'load-custom-electric-pairs)
+(defun load-custom-electric-pairs ()
+  (when (and (stringp buffer-file-name)
+	     (seq-contains-p '("php" "js" "py")
+			 (file-name-extension buffer-file-name)))
+    (setq electric-pair-pairs '((?\' . ?\')))))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :after company
+  :hook ((clojure-mode . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deffered)
+  :init (setq lsp-keymap-prefix "C-c l")
+  :config
+  (setq gc-cons-threshold 100000000
+        read-process-output-max (* 1024 1024)
+        lsp-completion-provider :capf
+	company-minimum-prefix-length 2
+	company-idle-delay 0.1))
+
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :commands lsp-ui-mode)
+
+(use-package yasnippet
+  :ensure t
+  :init
+  (progn
+    (yas-global-mode 1)))
+
+(use-package dap-mode
+  :ensure t)
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
+(use-package cider
+  :ensure t)
+
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(gruvbox-theme cider magit yasnippet which-key web-mode use-package try projectile neotree lsp-ui gruber-darker-theme flycheck editorconfig dap-mode counsel alchemist)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
